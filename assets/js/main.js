de46699adf058544d2875bdb3aa0994bffe3fabb
Bohdan <bohdan.verbetskyi@gmail.com>
2025-06-19 23:41:22 +0200

    Fix method getFullInfo on class Vehicle

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,12 @@ class Vehicle {
 		this._manufactureYear = manufactureYear
 	}
 
-	getFullInfo() {
-		return `${this._brand} ${this._model} (${this._manufactureYear}) - Year: ${this.Year}, `
+	get Year() {
+		return new Date().getFullYear() - this._manufactureYear
 	}
 
-	get Year() {
-		return new Date().getFullYear() - this.manufactureYear
+	getFullInfo() {
+		return `${this._brand} ${this._model} (${this._manufactureYear}) - Year: ${this.Year}, `
 	}
 }
 
